264073be4f7255786931569a0797e8476f2e336b
Muhammad Atif <atifahmadani68@gmail.com>
2025-08-15 11:43:24 +0500

    simple Calculator

---
+++ calculater.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+	float a,b;
+	cout<<"enter two numbers"<<endl;
+	cin>>a>>b;
+	int sum,multi,sub;
+	float divide;
+	cout<<"sum is:"<<a+b<<"\nsub is:"<<a-b<<"\nmutli is:"<<a*b<<"\ndivision is:"<<a/b<<endl;
+}

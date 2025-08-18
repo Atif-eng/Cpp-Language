e771ae28b2260fafe66efee6bef68620b18f2435
Muhammad Atif <atifahmadani68@gmail.com>
2025-08-18 09:05:35 +0500

    Commments

---
+++ comment.cpp
@@ -0,0 +1,12 @@
+// What is comment
+
+// Single-line comments start with two forward slashes (//).
+// the comment line is ignored by the compiler (will not be executed).
+// This example uses a single-line comment before a line of code:
+
+#include <iostream>
+using namespace std;
+int main(){
+//	this is comment
+	cout<<"Hi...I am Here"<<endl;
+}

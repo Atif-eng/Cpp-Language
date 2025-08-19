34448a35ef2702cbab881d8e972a8bae0ea071f7
Muhammad Atif <atifahmadani68@gmail.com>
2025-08-19 10:40:37 +0500

    Data Types

---
+++ data_type.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main(){
+//	Data Types in C++
+
+//  integer datatype
+	int a=12;
+	
+//	Float datatype
+	float temp=4.3;
+	
+//	character datatype
+	char c='A';
+	
+//	boolian datatype
+	bool is_active = true;
+
+//  string datatype	
+	string s = "Hello";
+	
+	
+//	Output of these datatype
+	cout<<a<<endl;
+	cout<<temp<<endl;
+	cout<<c<<endl;
+	cout<<is_active<<endl;
+	cout <<s<<endl;
+}

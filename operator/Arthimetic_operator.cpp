e49190ac9b53c636964aed2f0fed0a09a52d2067
Muhammad Atif <atifahmadani68@gmail.com>
2025-08-30 16:23:33 +0500

    Arthimetic operator

---
+++ operator/Arthimetic_operator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+//	Arthimatic operators
+	int sum = 100 + 50;
+	int sub=10-3;
+	int multi=3*5;
+	float divide=10/2;
+	
+//	output
+	cout<<"Sum: "<<sum<<endl;
+	cout<<"sub: "<<sub<<endl;
+	cout<<"multi: "<<multi<<endl;
+	cout<<"Divide: "<<divide<<endl;
+
+}
+

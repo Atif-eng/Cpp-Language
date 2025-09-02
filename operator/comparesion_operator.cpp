4db3a432148095f3a566742ee0aaaef43b5133ac
Muhammad Atif <atifahmadani68@gmail.com>
2025-09-02 10:43:59 +0500

    comparesion operators

---
+++ operator/comparesion_operator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+	cout<<"1=True and 0=False"<<endl;
+	
+	bool a=(3>2);
+	cout<<a<<endl;
+	
+	bool b=(3<2);
+	cout<<b<<endl;
+	
+	bool c=(4==3);
+	cout<<c<<endl;
+	
+	bool d=(4!=3);
+	cout<<d;
+	
+}
+

e886c231fcf1bf364e0382bf23855b9f1de1428a
Muhammad Atif <atifahmadani68@gmail.com>
2025-09-02 10:41:30 +0500

    Assignment Operator

---
+++ operator/Assihnment_operator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+	int x=3;
+	cout<<"x: "<<x<<endl;
+	
+	x+=2;
+	cout<<"x+=2: "<<x<<endl;
+	
+	x-=2;
+	cout<<"x-=2: "<<x<<endl;
+	
+	x*=3;
+	cout<<"x*=3: "<<x<<endl;
+}
+

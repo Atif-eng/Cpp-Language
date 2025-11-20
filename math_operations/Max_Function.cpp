b47cdd429c2438ca48f119e916c3a7962dee1c82
Muhammad Atif <atifahmadani68@gmail.com>
2025-11-20 08:02:39 +0500

    max_function
    max_function

---
+++ math_operations/Max_Function.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+	int num1,num2;
+	cout<<"Enter num1: ";
+	cin>>num1;
+	
+	cout<<"Enter num12: ";
+	cin>>num2;
+	
+	cout<<"Maximum of "<<num1<<" and "<<num2<<" is: "<<max(num1,num2)<<endl;
+}

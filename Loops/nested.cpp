69739a4ed8b8ed9b8b682987c2f6c363f5d7e6d2
Muhammad Atif <atifahmadani68@gmail.com>
2025-11-29 10:52:24 +0500

    A basic example of nested loops

---
+++ Loops/nested.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+	int n;
+	cout<<"Enter a number: ";
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<=n;i++){
+		cout<<i<<endl;
+		for(int j=0;j<=20;j++){
+			cout<<j<<endl;
+		}
+   }
+}

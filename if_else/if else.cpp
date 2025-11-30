4c0aa3571cb1560108d5f740caeaa8ee2217a50d
Muhammad Atif <atifahmadani68@gmail.com>
2025-11-30 22:40:50 +0500

    Add files via upload

---
+++ if_else/if else.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int a;
+	cout<<"enter a number:"<<endl;
+	cin>>a;
+	if(a<0){
+		cout<<"negative"<<endl;
+	}
+	else if(a==0){
+		cout<<"zero"<<endl;
+	}
+	else{
+	cout<<"positive"<<endl;
+    }
+	return 0;
+	
+}

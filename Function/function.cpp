6c5038ad62d786297bf94d1773a76ed1a83eab19
Muhammad Atif <atifahmadani68@gmail.com>
2025-12-05 10:25:10 +0500

    Add files via upload

---
+++ Function/function.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+void sum(int a=3,int b=1){
+	
+	cout<<"sum is:"<<a+b<<endl;
+}
+int main(){                              //main  function
+	cout<<"Hello I am here..."<<endl;
+	sum();
+//	cout<<"sum is:"<<sum();
+}
